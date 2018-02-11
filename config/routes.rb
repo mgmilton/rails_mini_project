f22071ca8fe82e1628a64d3a76d3608d7a071d6d
mgmilton <matt.milton@gmail.com>
2018-02-10 18:02:15 -0700

    Adds logout path and simple nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users, only: [:new, :create, :show]
   resources :ideas
   resources :categories do
     resources :ideas, only: [:new, :create, :show, :destroy, :update]
   end
-  
+
   namespace :admin do
     resources :categories, only: [:index]
   end

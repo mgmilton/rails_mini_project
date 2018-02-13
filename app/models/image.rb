2b518797d5d4a60c8ff9cc2861cf2d0f09f7e405
mgmilton <matt.milton@gmail.com>
2018-02-13 08:59:10 -0700

    updates dependent destroy

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
   validates :url, presence: true, uniqueness: true
-  has_many :idea_images, dependent: :destroy
+  has_many :idea_images, dependent: :nullify
   has_many :ideas, through: :idea_images
 end

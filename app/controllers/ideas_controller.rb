6b00416be59a845e7efe0eb2f7be5b2612dbf260
mgmilton <matt.milton@gmail.com>
2018-02-13 17:32:17 -0700

    styles ideas/_form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -62,7 +62,7 @@ class IdeasController < ApplicationController
   private
 
     def idea_params
-      params.require(:idea).permit(:title, :description, :category_id, image_ids: [])
+      params.require(:idea).permit(:title, :description, :category_id, {image_ids: []})
     end
 
     def set_idea

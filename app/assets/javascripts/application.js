efd1d4b513998ca5e9ec14fcf6ad051eff4b97bc
mgmilton <matt.milton@gmail.com>
2018-02-13 18:44:27 -0700

    Adds carousel feature to all ideas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,7 @@
 $(document).ready(function(){
        $('.dropdown-toggle').dropdown();
    });
+
+$(function(){
+     $('#myCarousel').carousel('cycle');
+   });
--- spec/features/ideas/user_can_delete_an_idea_spec.rb
+++ spec/features/ideas/user_can_delete_an_idea_spec.rb
@@ -5,13 +5,14 @@ describe "User visits idea index page" do
     it "allows default user to delete an idea" do
       user = create(:user)
       idea = create(:idea, user: user)
-
+      idea.images.create(name: 't', url: "http://i.telegraph.co.uk/multimedia/archive/03013/atlantic_3013875b.jpg")
+      
       visit login_path
 
       fill_in "email", with: user.email
       fill_in "password", with: user.password
       click_on "Log In Here"
-     
+
       visit user_ideas_path(user)
 
       click_link "Delete"
